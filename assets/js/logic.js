ed57f2a35cb600bda608459530b5860df7ce2701
Trai Nguyen <tmnguyen8@gmail.com>
2019-09-22 00:07:04 -0400

    added functions to display GIF

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,22 +2,45 @@
 // *********************************************************
 let searchList = [];
 let searchText = "";
+let queryURL = "";
 // FUNCTIONS
 // *********************************************************
+// Function for displaying button search text data
+function displayButtons() {
+    for (i of searchList) {
+        $("#searchView").append(`<button id="searchBtn" class="btn btn-secondary" data-search="${i}">${i}</button>`)
+    };
+};
 
+// function for displaying gif based on button selected
+function displayGif(){
+    $(".gifView").empty();
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
 
+    for (i of response.data) {
+      var image = $("<img>").attr("src", i.images.fixed_width.url);
+      $(".gifView").append(image)
+    }
+  })
+}
 // EXECUTION
 // *********************************************************
-$("#searchBtn").click(function() { 
-    searchText = $("#searchText").val();
-    $(".searchResult").append(`<button type="button" class="btn btn-success">${searchText}</button>`);
+// This function handles events where one button is clicked
+$("#addBtn").on("click", function(event) {
+    event.preventDefault();
+    searchList.push($("#searchText").val());
+    // clear the button-views div
+    $("#searchView").empty();
+    displayButtons();
+});
 
-    if (!searchList.includes(searchText)) {
-        searchList.push(searchText);
-    } else {
-        alert("The following search has already been selected.");
-    };
-
-
-    
-}); 
+$(document).on("click", "#searchBtn", function(event) {
+    event.preventDefault();
+    searchText = $(this).data("search");
+    queryURL = `https://api.giphy.com/v1/gifs/search?q=${searchText}&limit=25&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9`;
+    displayGif();
+});
