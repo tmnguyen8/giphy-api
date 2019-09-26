535ecc06351c99e8b56ac7fc3ec761bd0aae037a
Trai Nguyen <tnguyen@at-pac.com>
2019-09-26 10:11:23 -0400

    added button to play and pause/ style gifView

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -8,7 +8,12 @@ let queryURL = "";
 // Function for displaying button search text data
 function displayButtons() {
     for (i of searchList) {
-        $("#searchView").append(`<button id="searchBtn" class="btn btn-secondary" data-search="${i}">${i}</button>`)
+        $("#searchView").append(`
+        <div class="btn-wrapper">
+        <button id="searchBtn" class="btn btn-secondary" data-search="${i}">${i}</button>
+        <button id="deleteBtn" class="btn fa fa-close" data-search="${i}"></button>
+        </div>
+        `)
     };
 };
 
@@ -23,7 +28,16 @@ function displayGif(){
 
     for (i of response.data) {
       var image = $(`
-      <img src="${i.images.fixed_width.url}" class="col-12 col-md-6 col-lg-4 image">`);
+        <a href="${i.images.fixed_width.url}">
+          <div class="gif-wrapper" style="transform: translate3d(0px, 0px, 0px) background-color: rgb(233,236,239);">
+            <img src="${i.images.fixed_width.url}" class="gif-image" class="d">
+            <p class="card-text">Rating: ${i.rating}</p>
+            <button id="animateGifBtn" class="btn fa fa-play-circle" data-search="${i}" data-animate=""></button>
+
+          </div>
+        </a>
+      `);
+
       $(".gifView").append(image)
     }
   })
@@ -33,10 +47,23 @@ function displayGif(){
 // This function handles events where one button is clicked
 $("#addBtn").on("click", function(event) {
     event.preventDefault();
-    searchList.push($("#searchText").val());
+    var inputText = $("#inputText").val()
+    // Check if the searchText already exist;
+    if (searchList.includes(inputText)) {
+      alert("This search already exists in your list. Select another search term");
+    } else {
+      searchList.push(inputText);
+    }
     // clear the button-views div
     $("#searchView").empty();
     displayButtons();
+});
+
+$(document).on("click", "#clearBtn", function(event) {
+  event.preventDefault();
+  searchList = [];
+  $("#searchView").empty();
+  displayButtons();
 });
 
 $(document).on("click", "#searchBtn", function(event) {
@@ -44,4 +71,16 @@ $(document).on("click", "#searchBtn", function(event) {
     searchText = $(this).data("search");
     queryURL = `https://api.giphy.com/v1/gifs/search?q=${searchText}&limit=25&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9`;
     displayGif();
+});
+
+$(document).on("click", "#deleteBtn", function(event) {
+  event.preventDefault();
+  searchText = $(this).data("search");
+  // deletting selected searchText from list searchList
+  var deleteIndex = searchList.indexOf(searchText);
+  if (deleteIndex != -1) {
+    searchList.splice(deleteIndex, 1);
+  }
+  $("#searchView").empty();
+  displayButtons();
 });
